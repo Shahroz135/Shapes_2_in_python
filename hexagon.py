72d60e8614520a3019f1b2a26ab4c41e013cf2d8
Muhammad Shahroz <116105635+Shahroz135@users.noreply.github.com>
2023-07-02 20:26:13 +0500

    Add files via upload

---
+++ hexagon.py
@@ -0,0 +1,9 @@
+import turtle
+ms=turtle.Screen()
+ms.bgcolor('light green')
+ms.title("Hexagon")
+aq=turtle.Turtle()
+for s in range(36):
+	aq.forward(100)
+	aq.right(60)
+turtle.done()
---
+++ octagon.py
@@ -0,0 +1,10 @@
+import turtle
+wn=turtle.Screen()
+wn.title("octagon")
+wn.bgcolor("white")
+turtle.speed(7)
+ct=turtle.Turtle()
+for a in range(40):
+	ct.forward(100)
+	ct.left(45)
+turtle.done()
